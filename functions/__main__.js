2af020b2d2746e8967cd1f4edeb82e62c90b7758
Andrew Boyd <andrew@wearebraid.com>
2019-04-14 15:45:53 -0400

    only parse HTML if we have valid comments to match

--- functions/__main__.js
+++ functions/__main__.js
@@ -7,62 +7,67 @@ const lib = require('lib')({ token: process.env.STDLIB_TOKEN });
 * @returns {any}
 */
 module.exports = (subreddit = 'ethtrader', context, callback) => {
-  lib[`${context.service.identifier}.parseHTML`]({
-    url: 'https://www.reddit.com/r/ethtrader/'
-  }).then(banner_url => {
-    lib.utils.storage
-      .get('commentsRepliedTo')
-      .then(comments => {
-        return comments || {};
-      })
-      .then(commentsRepliedTo => {
-        reddit
-          .getSubreddit(subreddit)
-          .getNewComments()
-          .then(listing => {
-            let comments = listing.map(comment => {
-              return {
-                name: comment.name,
-                body: comment.body.toLowerCase()
-              };
-            });
-
-            let validComments = comments.filter(comment => {
-              return KEYWORDS.some(keyword => {
-                return (
-                  comment.body.indexOf(keyword) !== -1 && !(comment.name in commentsRepliedTo)
-                );
-              });
-            });
-
-            let replyPromises = validComments.map(comment => {
-              let response = `r/ethtrader community banner image link: [${banner_url}](${banner_url})`
-
-              return lib[`${context.service.identifier}.reply`]({
-                'parent': comment.name,
-                'text': response
-              }).then(result => {
-                return result;
-              });
-            });
-
-            return Promise.all(replyPromises).then(results => {
-              // return callback(null, results)
-              results.map(result => {
-                commentsRepliedTo[result.parent_id] = true;
-              });
-              lib.utils.storage
-                .set('commentsRepliedTo', commentsRepliedTo)
-                .then(_results => {
-                  return callback(null, results);
-                });
-            });
-          })
-          .catch(error => {
-            return callback(error);
+  lib.utils.storage
+    .get('commentsRepliedTo')
+    .then(comments => {
+      return comments || {};
+    })
+    .then(commentsRepliedTo => {
+      reddit
+        .getSubreddit(subreddit)
+        .getNewComments()
+        .then(listing => {
+          let comments = listing.map(comment => {
+            return {
+              name: comment.name,
+              body: comment.body.toLowerCase()
+            };
           });
-      });
-  });
+
+          let validComments = comments.filter(comment => {
+            return KEYWORDS.some(keyword => {
+              return (
+                comment.body.indexOf(keyword) !== -1 && !(comment.name in commentsRepliedTo)
+              );
+            });
+          });
+
+          let banner_url = '';
+          if (validComments.length > 0) {
+             return lib[`${context.service.identifier}.parseHTML`]({
+              url: 'https://www.reddit.com/r/ethtrader/'
+            }).then(banner_url => {
+              let replyPromises = validComments.map(comment => {
+                let response = `r/ethtrader community banner image link: [${banner_url}](${banner_url})`
+
+                return lib[`${context.service.identifier}.reply`]({
+                  'parent': comment.name,
+                  'text': response
+                }).then(result => {
+                  return result;
+                });
+              });
+
+              return Promise.all(replyPromises).then(results => {
+                // return callback(null, results)
+                results.map(result => {
+                  commentsRepliedTo[result.parent_id] = true;
+                });
+                lib.utils.storage
+                  .set('commentsRepliedTo', commentsRepliedTo)
+                  .then(_results => {
+                    return callback(null, results);
+                  });
+              });
+            })
+          } else {
+            return callback(null, 'no matching comments');
+          }
+        })
+        .catch(error => {
+          return callback(error);
+        });
+    });
 };
 
 const KEYWORDS = ['!bannerbot'];
